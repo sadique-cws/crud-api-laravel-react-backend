e08234c48b5624762a76946c1dfdc4b9780a8ed1
sadique-cws <sadique.hussain96@gmail.com>
2022-11-09 16:01:21 +0530

    first commit

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post("/login",[App\Http\Controllers\AuthController::class,"login"]);
+Route::post("/register",[App\Http\Controllers\AuthController::class,"register"]);
